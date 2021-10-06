1834615dfc3e95b3878070387827f6e3c97ff4d6
lokistarwars <89352187+lokistarwars@users.noreply.github.com>
2021-10-06 14:26:46 +0530

    Update start_miner_etc.bat

--- PhoenixMiner_5.6d_Windows - Copy/start_miner_etc.bat
+++ PhoenixMiner_5.6d_Windows - Copy/start_miner_etc.bat
@@ -9,6 +9,6 @@ setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
 REM IMPORTANT: Replace the ETC address with your own ETC wallet address in the -wal option (Rig001 is the name of the rig)
-PhoenixMiner.exe -pool ssl://eu1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.azr9tefjykgmpjj -gpow 96 -coin etc
+PhoenixMiner.exe -pool ssl://eu1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.56yzw41xdtldwh2 -gpow 96 -coin etc
 pause
 
